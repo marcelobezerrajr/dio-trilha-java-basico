2c2485d4eebd4f294bdba7bdb69f1430548180ca
Marcelo Bezerra <marcelojuniorbzerra12@gmail.com>
2025-03-18 11:56:34 -0300

    Adicionado resolucao do desafio, em que pede nome, numero da agencia, conta e saldo, para mostar uma frase posteriormente com esses dados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu Nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o número da sua Agência:");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Digite o número da sua Conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o valor do seu Saldo:");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
